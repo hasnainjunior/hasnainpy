01b91d83ca8587aa8d108a8809d85285abb10e72
hasnainjunior <hasnain19559@gmail.com>
2024-06-29 14:52:05 +0500

    Add files via upload

---
+++ leo.py
@@ -0,0 +1,17 @@
+#first
+height=10
+base=15
+f=0.5
+area=f*height*base
+print("area of tringle", area) 
+
+#second
+radius=7
+pie=3.14
+area=radius*pie
+print("area of circle", area)
+
+#third
+s=7
+area=s*s
+print("area of square", area)
